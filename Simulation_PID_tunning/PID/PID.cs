3e21f221a351c0fff5fa177872e2ac068bc459c0
Cristian Filimon <filimon.cristian.ionut@gmail.com>
2015-10-28 22:30:29 +0200

    PID
    reset and calculate PID are implemented. Calculate function needs to be
    reviewed. Use the FirstTime property. There is a condition on this
    variable.

--- Simulation_PID_tunning/PID/PID.cs
+++ Simulation_PID_tunning/PID/PID.cs
@@ -20,7 +20,44 @@ namespace Simulation_PID_tunning.PID
 
         public PID()
         {
-           
+            Integrated = 0;
+            PrevInput = 0;
+            FirstTime = true;
+        }
+
+        public void Reset()
+        {
+            Integrated = 0;
+            PrevInput = 0;
+            FirstTime = true;
+        }
+
+        public double Calculate(double desiredSetPoint, double actualValue)
+        {
+            var error = 0.0;
+            var result = 0.0;
+
+            //todo use the FirstTime property. There is a condition on this variable.
+            
+            error = desiredSetPoint - actualValue; //actual error
+
+                          //prop                 //derivative         //integral
+            result = (error * Kp) + ((error - PrevInput) * Kd) + Integrated * Ki;
+
+            PrevInput = error;
+            Integrated = Integrated + error; //calculated integrated value
+            
+
+            if (result > MaxOutput)
+            {
+                result = MaxOutput;
+            }
+            else if (result < MinOutput)
+            {
+                result = MinOutput;
+            }
+
+            return result;
         }
     }
 }
