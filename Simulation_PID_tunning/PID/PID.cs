b3f2ad5ffebbcd4cd01ca699c7344b34a980a8ef
Cristian Filimon <filimon.cristian.ionut@gmail.com>
2015-10-28 22:02:35 +0200

    PID
    Started PID class. Declaration of properties has begun.

---
+++ Simulation_PID_tunning/PID/PID.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation_PID_tunning.PID
+{
+    class PID
+    {
+        public double Kd { get; set; }
+        public double Ki { get; set; }
+        public double Kp { get; set; }
+
+        public double MinOutput { get; set; }
+        public double MaxOutput { get; set; }
+        public double Integrated { get; set; }
+        public double PrevInput { get; set; }
+        public bool FirstTime { get; set; }
+
+        public PID()
+        {
+           
+        }
+    }
+}
